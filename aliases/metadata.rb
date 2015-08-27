3f9e25acb6e7761c3c6a9dbf2d7338e7e9b66a08
Andrei Samuilik <a.samuylik@gmail.com>
2015-08-27 13:48:38 +0300

    Cookbooks have been added

---
+++ aliases/metadata.rb
@@ -0,0 +1,8 @@
+name             'aliases'
+maintainer       'Oleg Krook'
+maintainer_email 'okrook@remedypointsolutions.com'
+license          'Apache 2.0'
+description      'Installs/Configures aliases'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'magic_shell'
